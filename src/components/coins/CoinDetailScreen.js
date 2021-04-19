61cbe601fca9031f665a8c76cdd8484fae3afc8c
agusrs <39487421+agusrs@users.noreply.github.com>
2021-04-18 21:38:29 -0300

    First two screens, reactnative navigation

---
+++ src/components/coins/CoinDetailScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const CoinsScreen = () => {
+    return (
+        <View>
+            <Text>
+                Coin Detail Screen
+            </Text>
+        </View>
+    )
+}
+
+export default CoinsScreen
